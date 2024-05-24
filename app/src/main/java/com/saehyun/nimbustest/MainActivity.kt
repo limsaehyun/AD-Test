2718418523364c1304c2ab519fbf858077b1f15d
limsaehyun <sh00710000@gmail.com>
2024-05-24 15:23:55 +0900

    Add Nimbus

---
+++ app/src/main/java/com/saehyun/nimbustest/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.saehyun.nimbustest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun navigateToSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    fun onClick(view: View) {
+        navigateToSecondActivity()
+    }
+}
